a2fe2264d0268c1248712317e5cd9203502cf989
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 17:43:49 +0300

    kata - You only need one

--- codewars.js
+++ codewars.js
@@ -9,3 +9,19 @@ function century(year) {
   return centuryCount;
 }
 
+
+/*You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not. */
+
+function check(a, x) {
+  var i = a.length;
+  while (i--) {
+     if (a[i] === x) {
+         return true;
+     }
+  }
+  return false;
+}
