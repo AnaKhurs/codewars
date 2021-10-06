d6d3869f902a77a49be35526862d99b6ee5d66bc
AnaKhurs <ana.khurs@gmail.com>
2021-10-06 20:31:37 +0300

    kata Square(n) Sum

--- codewars.js
+++ codewars.js
@@ -52,3 +52,15 @@ function hero(bullets, dragons){
     return false;
   }
   }
+
+  //Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+  function squareSum(numbers){
+    let sum = 0;
+  for (let i = 0; i < numbers.length; i++){
+    sum += numbers[i]*numbers[i];
+  }
+     return sum;
+  }
+
+  
