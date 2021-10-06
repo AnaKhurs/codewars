aa1ed63b0414efed8450e572e6e488035478a58e
AnaKhurs <ana.khurs@gmail.com>
2021-10-06 20:40:32 +0300

    kata Opposites Attract

--- codewars.js
+++ codewars.js
@@ -63,4 +63,13 @@ function hero(bullets, dragons){
      return sum;
   }
 
-  
+  //Opposites Attract
+  function lovefunc(flower1, flower2){
+    if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+      return true;
+    } else if (flower1 % 2 === 1 && flower2 % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
