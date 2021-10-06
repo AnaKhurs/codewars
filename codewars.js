3246cdfc24f6c1b3a215f8d50acbf300f1f7c462
AnaKhurs <ana.khurs@gmail.com>
2021-10-06 21:45:03 +0300

    kata Sum of positive

--- codewars.js
+++ codewars.js
@@ -104,3 +104,14 @@ function hero(bullets, dragons){
     function opposite(number) {
       return number * -1;
     }
+
+    //Sum of positive
+    function positiveSum(arr) {
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++){
+        if (arr[i] > 0) {
+          sum += arr[i];
+        }
+      } return sum;
+    }
+    
