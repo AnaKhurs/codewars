0c49ea86005a2edb8ecbb2095079faa40edebd20
AnaKhurs <ana.khurs@gmail.com>
2021-10-06 21:49:46 +0300

    kata  Convert boolean values to strings 'Yes' or 'No'.

--- codewars.js
+++ codewars.js
@@ -115,3 +115,12 @@ function hero(bullets, dragons){
       } return sum;
     }
     
+
+    // Convert boolean values to strings 'Yes' or 'No'.
+    function boolToWord( bool ){
+      if (bool == true){
+        return "Yes";
+      } else {
+        return "No";
+      }
+    }
