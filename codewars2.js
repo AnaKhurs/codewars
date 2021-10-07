6fe315a085876b8c512d1fd7b744b45fa8454a6b
AnaKhurs <ana.khurs@gmail.com>
2021-10-07 21:09:14 +0300

    Basic variable assignment

--- codewars2.js
+++ codewars2.js
@@ -2,3 +2,9 @@
 function simpleMultiplication(number) {
   return r = number % 2 === 0 ? number * 8 : number * 9;
 }
+
+
+// Basic variable assignment
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
