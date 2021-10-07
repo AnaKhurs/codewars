f6ae047b47b49650b599a7c21beebabcf84cd7cd
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-07 16:52:57 +0300

    How good are you really?

--- codewars1.js
+++ codewars1.js
@@ -62,3 +62,11 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return false;
   }
 };
+
+// How good are you really?
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  const average = classPoints.reduce((x, y) => x + y, 0) / classPoints.length;
+  
+  return average > yourPoints ? false : true
+}
