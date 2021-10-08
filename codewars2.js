b2f32b00a4ee1c916dccc7f1e53831352bc234d5
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-08 09:39:19 +0300

    Grasshopper - If/else syntax debug

--- codewars2.js
+++ codewars2.js
@@ -89,3 +89,12 @@ function feast(beast, dish) {
       }
       
     }
+
+    //Grasshopper - If/else syntax debug
+    function checkAlive (health) {
+      if (health > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
