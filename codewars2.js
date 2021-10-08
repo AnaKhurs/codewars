6b22018185ba95661015d0a54312ba8f41f89fa5
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-08 10:55:35 +0300

    Beginner Series #1 School Paperwork

--- codewars2.js
+++ codewars2.js
@@ -124,3 +124,13 @@ function feast(beast, dish) {
       let c = a + b;
       return String(c);
     }
+
+    //Beginner Series #1 School Paperwork
+    function paperwork(n, m) {
+      if (n < 0 || m < 0) {
+        return 0
+      } else {
+        return n * m;
+      }
+    }
+    
