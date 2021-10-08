ccf59d23d113bbbd437bc4c854af34ca20536872
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-08 09:30:03 +0300

    Quarter of the year

--- codewars2.js
+++ codewars2.js
@@ -75,3 +75,17 @@ function feast(beast, dish) {
     function subt(a,b){
         return a - b;
     }
+
+    //Quarter of the year
+    const quarterOf = (month) => {
+      if (month <= 3 && month >= 1 ){
+        return 1;
+      } else  if (month <= 6 && month >= 4 ){
+        return 2;
+      } else  if (month <= 9 && month >= 7 ){
+        return 3;
+      } else  if (month <= 12 && month >= 10 ){
+        return 4;
+      }
+      
+    }
