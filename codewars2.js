3bc475076b0cda5cfd5d37d626ff387862f35094
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-08 11:38:21 +0300

    What's the real floor?

--- codewars2.js
+++ codewars2.js
@@ -133,4 +133,15 @@ function feast(beast, dish) {
         return n * m;
       }
     }
+
+    //What's the real floor?
+    function getRealFloor(n) {
+      if (n <= 0){
+        return n;
+      } else if (n > 0 && n <= 13 ){
+        return n - 1;
+      } else if (n > 13 ){
+        return n - 2;
+      } 
+     }
     
