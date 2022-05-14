b18b033897b70cbcb066166059dfbd260ca08dfc
AnaKhurs <ana.khurs@gmail.com>
2022-05-14 19:08:42 +0300

    cata Square Every Digit

--- codewars71-100.js
+++ codewars71-100.js
@@ -2,3 +2,7 @@
 function isTriangle(a,b,c){
     return (a+b > c && a+c > b && c+b > a) ? true : false
 }
+//72 Square Every Digit
+function squareDigits(num){
+    return +num.toString().split('').map(i => i*i).join('')
+}
