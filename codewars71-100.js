427741e39cefc95d84fce812dbe5aca5b37c1eda
AnaKhurs <ana.khurs@gmail.com>
2022-05-20 21:53:09 +0300

    cata Cat years, Dog years

--- codewars71-100.js
+++ codewars71-100.js
@@ -39,3 +39,21 @@ function digitize(n) {
 }
 
 console.log(digitize(348597));
+
+//76 Cat years, Dog years
+var humanYearsCatYearsDogYears = function(humanYears) {
+
+    let catYears = 15
+    let dogYears = 15
+
+    if(humanYears > 2){
+        catYears = 15 + 9 + (humanYears-2)*4
+        dogYears  = 15 + 9 + (humanYears-2)*5
+    }
+    if(humanYears === 2){
+        catYears = 15 + 9
+        dogYears  = 15 + 9
+    }
+
+    return [humanYears,catYears,dogYears];
+}
